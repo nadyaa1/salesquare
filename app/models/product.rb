755452cc9928f3d0f074827c05c52c443b153ac7
simone de best <simone.1994@hotmail.com>
2016-06-17 14:23:12 +0200

    product new is done, only validation message doesn't work

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   belongs_to :user
 
+  validates :name, :category, :price, :url, :photo, :description, presence: {message: "Field is required"}
   mount_uploader :photo, PhotoUploader
 end
