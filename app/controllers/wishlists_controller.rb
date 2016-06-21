e498fc3bfa5018a6f068fe77edda15a18b6bd603
Abdurrahman uymaz <appie_u_@hotmail.com>
2016-06-21 13:43:17 +0200

    delete a product from wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -3,4 +3,11 @@ class WishlistsController < ApplicationController
   def index
     @wishlists = current_user.wishlists
   end
+
+  def destroy
+    @wishlist_product = WishlistProduct.find(params[:id])
+    @wishlist_product.destroy
+    redirect_to account_wishlists_path
+  end
+
 end
