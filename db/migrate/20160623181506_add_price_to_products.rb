91bee42b32034ba8f8fdd21bb8727c2a6ac29ca6
Nadya Kalantary <nadyakalantary@gmail.com>
2016-06-24 17:30:33 +0200

    sign up form fe update

--- db/migrate/20160623181506_add_price_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddPriceToProducts < ActiveRecord::Migration
-  def change
-    add_monetize :products, :price, currency: { present: false }
-    # add_monetize :products, :optional_price, amount: { null: true, default: nil }
-  end
-end
