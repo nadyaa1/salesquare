f6c68a46e4a65b9a0fb1c1c35e321e763a8b07fa
Abdurrahman uymaz <appie_u_@hotmail.com>
2016-06-29 14:49:58 +0200

    all tags are working now

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,40 @@ class ProductsController < ApplicationController
       @products = @products.where(category: "dresses")
     elsif @category == "coats"
       @products = @products.where(category: "coats")
+    elsif @category == "blazers"
+      @products = @products.where(category: "blazers")
+    elsif @category == "jackets"
+      @products = @products.where(category: "jackets")
+    elsif @category == "jeans"
+      @products = @products.where(category: "jeans")
+    elsif @category == "lingerie"
+      @products = @products.where(category: "lingerie")
+    elsif @category == "shirts & blouses"
+      @products = @products.where(category: "shirts & blouses")
+    elsif @category == "skirts"
+      @products = @products.where(category: "skirts")
+    elsif @category == "socks & tights"
+      @products = @products.where(category: "socks & tights")
+    elsif @category == "baby gifts"
+      @products = @products.where(category: "baby gifts")
+    elsif @category == "accesories"
+      @products = @products.where(category: "accesories")
+    elsif @category == "jackets & coats"
+      @products = @products.where(category: "coats")
+    elsif @category == "hoodies & sweatshirts"
+      @products = @products.where(category: "hoodies & sweatshirts")
+    elsif @category == "shirts"
+      @products = @products.where(category: "shirts")
+    elsif @category == "cardigans & jumpsuits"
+      @products = @products.where(category: "cardigans & jumpsuits")
+    elsif @category == "trousers"
+      @products = @products.where(category: "trousers")
+    elsif @category == "shorts"
+      @products = @products.where(category: "shorts")
+    elsif @category == "t-shirts & vests"
+      @products = @products.where(category: "t-shirts & vests")
+    elsif @category == "socks"
+      @products = @products.where(category: "socks")
     end
   end
 
