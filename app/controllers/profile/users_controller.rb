2d59eeb3526193adf7cfc4e1e2d7cd337b71fa23
simone de best <simone.1994@hotmail.com>
2016-06-30 17:26:40 +0200

    wishlist and photo upload button

--- app/controllers/profile/users_controller.rb
+++ app/controllers/profile/users_controller.rb
@@ -2,4 +2,20 @@ class Profile::UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def update
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to profile_user_path
+    else
+      render 'show'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo, :photo_cache)
+  end
 end
